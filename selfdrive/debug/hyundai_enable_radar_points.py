912f0586514d9d53f83da9d859dc1731b3694a9b
Jon Jander <jon.jander@ptj.se>
2021-09-22 17:29:29 +0200

    always write

--- selfdrive/debug/hyundai_enable_radar_points.py
+++ selfdrive/debug/hyundai_enable_radar_points.py
@@ -77,28 +77,24 @@ if __name__ == "__main__":
   fw_version_data_id : DATA_IDENTIFIER_TYPE = 0xf100 # type: ignore
   fw_version = uds_client.read_data_by_identifier(fw_version_data_id)
   print(fw_version)
-  if fw_version not in SUPPORTED_FW_VERSIONS.keys():
-    print("radar not supported! (aborted)")
-    sys.exit(1)
+  # if fw_version not in SUPPORTED_FW_VERSIONS.keys():
+  #   print("radar not supported! (aborted)")
+  #   sys.exit(1)
 
   print("[GET CONFIGURATION]")
   config_data_id : DATA_IDENTIFIER_TYPE = 0x0142 # type: ignore
   current_config = uds_client.read_data_by_identifier(config_data_id)
   new_config = SUPPORTED_FW_VERSIONS[fw_version]["default_config" if args.default else "tracks_enabled"]
   print(f"current config: 0x{current_config.hex()}")
-  if current_config != new_config:
-    print("[CHANGE CONFIGURATION]")
-    print(f"new config:     0x{new_config.hex()}")
-    uds_client.write_data_by_identifier(config_data_id, new_config)
-    if not args.default and current_config != SUPPORTED_FW_VERSIONS[fw_version]["default_config"]:
-      print("\ncurrent config does not match expected default! (aborted)")
-      sys.exit(1)
+  # if current_config != new_config:
+  print("[CHANGE CONFIGURATION]")
+  print(f"new config:     0x{new_config.hex()}")
+  uds_client.write_data_by_identifier(config_data_id, new_config)
+  if not args.default and current_config != SUPPORTED_FW_VERSIONS[fw_version]["default_config"]:
+    print("\ncurrent config does not match expected default! (aborted)")
+    sys.exit(1)
 
-    print("[DONE]")
-    print("\nrestart your vehicle and ensure there are no faults")
-    if not args.default:
-      print("you can run this script again with --default to go back to the original (factory) settings")
-  else:
-    print("[DONE]")
-    print("\ncurrent config is already the desired configuration")
-    sys.exit(0)
+  print("[DONE]")
+  print("\nrestart your vehicle and ensure there are no faults")
+  if not args.default:
+    print("you can run this script again with --default to go back to the original (factory) settings")
