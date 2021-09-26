2c1c4cd7484019e80a7052f9dd8187f8ae239bfa
Jon Jander <jon.jander@ptj.se>
2021-09-26 14:00:26 +0200

    dont spam present

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -274,7 +274,7 @@ class CarInterface(CarInterfaceBase):
 
   @staticmethod
   def init(CP, logcan, sendcan):
-    if CP.openpilotLongitudinalControl and False:
+    if CP.openpilotLongitudinalControl:
       disable_ecu(logcan, sendcan, addr=0x7d0, com_cont_req=b'\x28\x83\x01')
 
   def update(self, c, can_strings):
