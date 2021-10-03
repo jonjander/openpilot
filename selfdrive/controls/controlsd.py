6ad3415e8493501d0e8ae9a3b09592f9b6a65609
Jon Jander <jon.jander@ptj.se>
2021-10-03 15:51:07 +0200

    Enable radar points on kia niro

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -115,6 +115,31 @@ class Controls:
     cp_bytes = self.CP.to_bytes()
     params.put("CarParams", cp_bytes)
     put_nonblocking("CarParamsCache", cp_bytes)
+    print("Try to enable radar tracks")
+    if self.CP.carFingerprint in [HYUNDAI_CAR.KIA_NIRO_EV]:
+      rdr_fw = None
+      for fw in self.CP.carFw:
+        if fw.ecu == "fwdRadar":
+          rdr_fw = fw
+          break
+      print(f"Found fwdRadar: {rdr_fw.fwVersion}")
+      if rdr_fw.fwVersion in [b'\xf1\x8799110Q4500\xf1\x00DEev SCC F-CUP      1.00 1.00 99110-Q4500         \xf1\xa01.00'] or True:
+        for i in range(40):
+          try:
+            query = IsoTpParallelQuery(self.pm.sock['sendcan'], self.can_sock, 0, [rdr_fw.address], [b'\x10\x07'], [b'\x50\x07'], debug=True)
+            for addr, dat in query.get_data(0.1).items(): # pylint: disable=unused-variable
+              print("ecu write data by id ...")
+              new_config = b"\x00\x00\x00\x01\x00\x01"
+              dataId = b'\x01\x42'
+              WRITE_DAT_REQUEST = b'\x2e'
+              WRITE_DAT_RESPONSE = b'\x68'
+              query = IsoTpParallelQuery(self.pm.sock['sendcan'], self.can_sock, 0, [rdr_fw.address], [WRITE_DAT_REQUEST+dataId+new_config], [WRITE_DAT_RESPONSE], debug=True)
+              query.get_data(0)
+              print(f"Retry {i+1}")
+              break
+            break
+          except Exception as e:
+            print(f"Failed {i}: {e}") 
 
     self.CC = car.CarControl.new_message()
     self.AM = AlertManager()
