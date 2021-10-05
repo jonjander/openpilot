52de7bebab592df225e455816cebb343d26dad95
Jon Jander <jon.jander@ptj.se>
2021-10-05 20:48:25 +0200

    saftyparam

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -272,13 +272,13 @@ class CarInterface(CarInterfaceBase):
 
     ret.enableBsm = 0x58b in fingerprint[0]
 
-    # if ret.openpilotLongitudinalControl:
-    #   if candidate in [CAR.HYUNDAI_GENESIS, CAR.IONIQ_EV_2020, CAR.IONIQ_EV_LTD, CAR.IONIQ_PHEV, CAR.IONIQ, CAR.KONA_EV, CAR.KIA_SORENTO,
-    #                    CAR.SONATA_LF, CAR.KIA_NIRO_EV, CAR.KIA_OPTIMA, CAR.VELOSTER, CAR.KIA_STINGER,
-    #                    CAR.GENESIS_G70, CAR.GENESIS_G80, CAR.KIA_CEED, CAR.ELANTRA]:
-    #     ret.safetyParam |= Panda.FLAG_HYUNDAI_LEGACY_LONG
-    #   else:
-    #     ret.safetyParam |= Panda.FLAG_HYUNDAI_LONG
+    if ret.openpilotLongitudinalControl:
+      if candidate in [CAR.HYUNDAI_GENESIS, CAR.IONIQ_EV_2020, CAR.IONIQ_EV_LTD, CAR.IONIQ_PHEV, CAR.IONIQ, CAR.KONA_EV, CAR.KIA_SORENTO,
+                       CAR.SONATA_LF, CAR.KIA_NIRO_EV, CAR.KIA_OPTIMA, CAR.VELOSTER, CAR.KIA_STINGER,
+                       CAR.GENESIS_G70, CAR.GENESIS_G80, CAR.KIA_CEED, CAR.ELANTRA]:
+        ret.safetyParam |= Panda.FLAG_HYUNDAI_LEGACY_LONG
+      else:
+        ret.safetyParam |= Panda.FLAG_HYUNDAI_LONG
       
     return ret
 
