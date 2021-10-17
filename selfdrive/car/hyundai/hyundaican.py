f5b2ac545dbb7d1f591aeea4d937a5a4cd319213
Jon Jander <jon.jander@ptj.se>
2021-10-17 21:58:08 +0200

    fca11_values

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -119,8 +119,8 @@ def create_acc_commands(packer, enabled, accel, jerk, idx, lead_visible, set_spe
     # seems to count 2,1,0,3,2,1,0,3,2,1,0,3,2,1,0,repeat...
     # (where first value is aligned to Supplemental_Counter == 0)
     # test: [(idx % 0xF, -((idx % 0xF) + 2) % 4) for idx in range(0x14)]
-    "CR_FCA_Alive": ((-((idx % 0xF) + 2) % 4) << 2) + 1,
-    "Supplemental_Counter": idx % 0xF,
+    "CR_FCA_Alive": idx % 0xF, # -(((idx) % 0xf) + (1 if idx == 2 else 0)) % 0x10, #((-((idx % 0xF) + 2) % 4) << 2) + 1,
+    "Supplemental_Counter": 0 #idx % 0xF,
   }
   fca11_dat = packer.make_can_msg("FCA11", 0, fca11_values)[2]
   fca11_values["CR_FCA_ChkSum"] = 0x10 - sum([sum(divmod(i, 16)) for i in fca11_dat]) % 0x10
